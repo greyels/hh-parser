aabbfb9421d8b1c914cc775a690fa2a8e2aa8589
greyels <greyels@gmail.com>
2019-04-22 20:54:27 +0300

    Exceptions handling has been added

--- hh_parser.py
+++ hh_parser.py
@@ -4,16 +4,17 @@ import statistics as st
 import sys
 
 API_URL = 'https://api.hh.ru/vacancies'
+AREA_DICT = {'Moscow': 1, 'NN': 66, 'Russia': 113}
 PER_PAGE = '100'
 PAGES = 21
 USD_RATE = 63.89
 EUR_RATE = 71.80
 
-def get_salaries(url, vacancy, area):
+def get_salaries(url, vacancy, area_number):
     salaries = []
     try:
         for page in range(PAGES):
-            param = {'text': vacancy, 'only_with_salary': 'true', 'area': area,
+            param = {'text': vacancy, 'only_with_salary': 'true', 'area': area_number,
                      'per_page': PER_PAGE, 'page': str(page)}
             r = requests.get(url, param).json()
             for i in r['items']:
@@ -45,22 +46,33 @@ def refine_salaries(salaries):
         ref_salaries.append(salary)
     return ref_salaries
 
+def get_area_number_by_name(area_name):
+    try:
+        assert area_name in AREA_DICT
+    except AssertionError:
+        available_names = []
+        for key in AREA_DICT:
+            available_names.append(key)
+        raise AssertionError ("Incorrect area! Please specify one of the next areas: "
+                              + str(available_names) + ".")
+    return AREA_DICT.get(area_name)
+
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: " + __file__ + " <vacancy> <Russia/Moscow/NN>")
+    try:
+        assert len(sys.argv) == 3, "Usage: " + __file__ + " <vacancy> <Russia/Moscow/NN>"
+        vacancy = sys.argv[1]
+        area_number = get_area_number_by_name(sys.argv[2])
+        salaries = get_salaries(API_URL, vacancy, area_number)
+        salaries = remove_bad_currencies(salaries)
+        salaries = refine_salaries(salaries)
+        average_salary = sum(salaries)/len(salaries)
+        median_salary = st.median(salaries)
+        print("Found " + str(len(salaries)) + " vacancies of '" + vacancy + "' in " + sys.argv[2] + ".\n"
+              "Average salary for '" + vacancy + "' is " + str(round(average_salary,2)) + " RUR.\n"
+              "Median salary for '" + vacancy + "' is " + str(median_salary) + " RUR.")
+    except AssertionError as error:
+        print(error)
         return 1
-    VACANCY = sys.argv[1].replace('+', ' ')
-    if sys.argv[2] == 'Moscow': AREA = 1
-    elif sys.argv[2] == 'NN': AREA = 66
-    else: AREA = 113
-    salaries = get_salaries(API_URL, VACANCY, AREA)
-    salaries = remove_bad_currencies(salaries)
-    salaries = refine_salaries(salaries)
-    average_salary = sum(salaries)/len(salaries)
-    median_salary = st.median(salaries)
-    print("Found " + str(len(salaries)) + " vacancies of '" + VACANCY + "' in " + sys.argv[2] + ".\n"
-          "Average salary for '" + VACANCY + "' is " + str(round(average_salary,2)) + " RUR.\n"
-          "Median salary for '" + VACANCY + "' is " + str(median_salary) + " RUR.")
     return 0
 
 if __name__ == '__main__':
